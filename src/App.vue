7e4c6c753dae6c2c9580bb7acd2fc2f7cf2fe19f
muhohoweb <muhohokenya@users.noreply.github.com>
2024-04-11 19:59:06 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Data from API:</h1>
+    <div v-for="item in data" :key="item.id">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'FetchData',
+  setup() {
+    const data = ref(null);
+
+    let baseUrl = "https://devops-todo-api.azurewebsites.net"
+    const fetchData = async () => {
+      const response = await fetch(baseUrl + '/api/TodoItems');
+      data.value = await response.json();
+      console.log(data)
+    };
+
+    onMounted(fetchData);
+
+    return { data };
+  },
+};
+</script>
